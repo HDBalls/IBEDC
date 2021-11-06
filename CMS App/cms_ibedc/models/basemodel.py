146219bef8559c2ef1fdc0ce0d313dfcd51703e3
PyBool <ekoemmanueljavl@gmail.com>
2021-11-06 07:57:22 -0700

    newfields_added

---
+++ CMS App/cms_ibedc/models/basemodel.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# This model Inherits from the Users Model
+
+from odoo import api, models, modules,fields, models
+
+class BaseDeclarative(models.Model):
+    _inherit = 'res.partner'
+
+    account_no = fields.Char(string='Account Number',size=50)
+    # meter_no = fields.Many2one('res.partner',string='bus_terminal_point') related to account_no, not to be used here
+    last_vending = fields.Date(string='Last vend date')
+    last_vend_amount = fields.Char(string='Last vend amount')
+    account_type = fields.Char(string='Account type')
+    billing_history = fields.One2many('billing.history','bill_root_id',readonly=True)
+    payment_history = fields.One2many('payment.history','payment_root_id',readonly=True)
+    customer_complaints = fields.One2many('customer.complaints','complaints_root_id',readonly=True)
+
+# Command to use When inheriting and adding new field to res.partners
+# python odoo-bin -r Eko@plexada -w 10111011qwe --conf odoo.conf -u ibedc_contacts
--- CMS App/cms_ibedc/models/complaints_history.py
+++ CMS App/cms_ibedc/models/complaints_history.py
@@ -1,1 +1,17 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
 
+from odoo import api, models, modules,fields, models
+
+class Complaints(models.Model):
+    _name = 'customer.complaints'
+    complaints_root_id = fields.Many2one('res.partner')
+    ticket_id = fields.Char(string='Ticket ID')
+    category = fields.Char(string='Category',required=True)
+    sub_category = fields.Char(string='Sub Category',required=True)
+    summary = fields.Char(string='Summary',required=True)
+    description = fields.Text(string='Description',required=True)
+    address = fields.Char(string='Address',required=True)
+    status = fields.Char(string='Status',required=True)
+    date_created = fields.Date(string='Date Created',required=True)
+    
