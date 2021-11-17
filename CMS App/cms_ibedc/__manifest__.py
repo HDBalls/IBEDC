4360efe26ba2918ad390935b1366ed587c66d6b6
PyBool <ekoemmanueljavl@gmail.com>
2021-11-17 15:35:03 -0800

    added permissions and updated hide.xml

--- CMS App/cms_ibedc/__manifest__.py
+++ CMS App/cms_ibedc/__manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 {
-    'name': 'IBEDC Contacts',
+    'name': 'Ibedc Contacts',
     'category': 'Sales/CRM',
     'sequence': -100,
     'summary': 'Centralize your address book',
